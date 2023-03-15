c2e366b1425874211f39ba06030313a6b4001203
Hatena10 <indiaoa1010@gmail.com>
2023-03-15 15:59:32 +0900

    0315

--- src/ch09/sec03/exam01/A.java
+++ src/ch09/sec03/exam01/A.java
@@ -1,5 +1,12 @@
 package ch09.sec03.exam01;
 
 public class A {
-
+	//인스턴스 멤버 클래스
+	static class B{}
+		
+	//인스턴스 필드 값으로 B객체 대입
+	B field1 = new B();
+	
+	//정적 필드 값으로 B 객체 대입
+	}
 }
---
+++ src/ch13/lecture/C03WildCard.java
@@ -0,0 +1,5 @@
+package ch13.lecture;
+
+public class C03WildCard {
+
+}
